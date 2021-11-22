d0f320ad477083cc115c44cf10c430b89233e56d
Mickey253 <jacob_l._miller@hotmail.com>
2021-11-21 17:21:39 -0700

    Added feature generation.

--- generation-script.py
+++ generation-script.py
@@ -17,25 +17,33 @@ from graph_functions import get_distance_matrix
 
 #Save object
 
+def draw_graph(G,X):
+    # Convert layout to vertex property
+    pos = G.new_vp('vector<float>')
+    pos.set_2d_array(X.T)
+
+    # Show layout on the screen
+    gt.graph_draw(G, pos=pos)
+
+
+
 n = 1
 for i in range(n):
     G = gt.load_graph("test_graph.dot")
-    for i in G.edges():
-        print(i)
+
 
     d = get_distance_matrix(G)
     #d = np.array(d)
 
     Y = myMDS(d,weighted=False)
     Y.solve(15)
-    #print(Y.X)
-    #output_euclidean(G,Y.X)
+    draw_graph(G,Y.X)
 
     #Compute all features
     stress = calc_stress(Y.X,d)
     neighborhood = calc_neighborhood(Y.X,d)
     edge_crossings = calc_edge_crossings(Y.X)
     angular_resolution = calc_angular_resolution(G,Y.X)
-    avg_edge_length = calc_edge_lengths(G,Y.X)
+    avg_edge_length = calc_edge_lengths(G.edges(),Y.X)
 
     #Save
