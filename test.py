1e6705882feb554b36d4623970c60110d9fe6dc2
Mickey253 <jacob_l._miller@hotmail.com>
2021-12-07 21:21:26 -0700

    Cleaned noises

--- test.py
+++ test.py
@@ -5,27 +5,8 @@ import numpy as np
 from SGD_MDS import all_pairs_shortest_path,myMDS
 from graph_functions import get_distance_matrix,get_tsnet_layout
 
-with open('data/test_collection.pkl', 'rb') as myfile:
+import os
 
-    myobj = pickle.load(myfile)
-
-def draw_graph(G,X,fname=None):
-    # Convert layout to vertex property
-    pos = G.new_vp('vector<float>')
-    pos.set_2d_array(X.T)
-    if fname:
-        gt.graph_draw(G, pos=pos,output='drawings/' + fname)
-    else:
-        # Show layout on the screen
-        gt.graph_draw(G, pos=pos)
-
-
-G = nx.random_partition_graph([20,20,20,20,20],0.8,0.01)
-d = np.array(all_pairs_shortest_path(G))
-H = gt.Graph(directed=False)
-H.add_vertex(n=len(G.nodes()))
-for e in G.edges():
-    H.add_edge(e[0],e[1])
-
-print(gt.global_clustering(H))
-#nx.write_graphml(G,"test.xml")
+path = 'data/noise'
+for filename in os.listdir(path):
+    print(filename)
